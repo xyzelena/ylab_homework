64e70d2afcd338a875573b9dd077bc74a32009db
Elena <emkhuzhina@gmail.com>
2024-09-06 08:45:45 +0530

    add rules

--- task1/src/locales/ru.js
+++ task1/src/locales/ru.js
@@ -1,13 +1,24 @@
 export default {
-    translation: {
-      baseTextUI: {
-        header: 'Header',
-      },
+  translation: {
+    baseTextUI: {
+      header: 'App',
+    },
 
-      buttons: {
-        btnExit: 'Выйти',
-        btnEnter: 'Войти',
-      },
+    buttons: {
+      btnExit: 'Выйти',
+      btnEnter: 'Войти',
+    },
 
-    }
+    loginForm: {
+      header: 'Войти',
+      email: 'email',
+      password: 'пароль',
+      invalidCredentials: 'Неверные имя пользователя или пароль',
+    },
+
+    main: {
+      descr: 'Поздравляем, вы вошли в Личный кабинет!',
+    },
+
+  }
 }
